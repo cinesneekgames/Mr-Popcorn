2f00c30abe1b79f38248a287e650ba140aa4db41
cinesneekgames <cinesneekgames@outlook.com>
2016-10-03 14:40:47 +0200

    Upload
    Upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public GameObject player;
+	public Transform spawn;
+
+	static public bool detached;
+
+	void Update () {
+		detachClaw ();
+	}
+
+	void detachClaw () {
+		if (!detached) {
+			if (Input.GetKeyDown ("space")) {
+				print ("Dropped!");
+				detached = true;
+				PlayerManager.rb.gravityScale = 1;
+				PlayerManager.rb.isKinematic = false;
+			}
+		}
+	}
+
+	public void RespawnPlayer() {
+		Instantiate (player, spawn.position, spawn.rotation);
+		detached = false;
+	}
+}
